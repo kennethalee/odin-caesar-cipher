3031bc02e4b9c633c9154719bc82dd4eaef0a731
Kenneth Aidan Lee <158255466+kennethalee@users.noreply.github.com>
2024-12-13 17:05:28 +0900

    Refactor code to comply with style guidelines

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,29 +1,29 @@
-  def encrypt_message(text, shift_factor)
-    right_shifted = []
-    text.each_char do |char|
-      if ("a".."z").include?(char)
-        new_char = ((char.ord - "a".ord + shift_factor) % 26 + "a".ord).chr
-        right_shifted << new_char
-      elsif ("A".."Z").include?(char)
-        new_char = ((char.ord - "A".ord + shift_factor) % 26 + "A".ord).chr
-        right_shifted << new_char
-      else
-        right_shifted << char
-      end
+def encrypt_message(text, shift_factor)
+  right_shifted = []
+  text.each_char do |char|
+    if ('a'..'z').include?(char)
+      new_char = ((char.ord - 'a'.ord + shift_factor) % 26 + 'a'.ord).chr
+      right_shifted << new_char
+    elsif ('A'..'Z').include?(char)
+      new_char = ((char.ord - 'A'.ord + shift_factor) % 26 + 'A'.ord).chr
+      right_shifted << new_char
+    else
+      right_shifted << char
     end
-    return right_shifted.join("")
   end
+  right_shifted.join('')
+end
 
-  # Prompt
-  print "Message to encrypt: "
-  message_encrypt = gets.chomp.to_s
+# Prompt
+print 'Message to encrypt: '
+message_encrypt = gets.chomp.to_s
 
-  begin
-    print "Enter right shift factor: "
-    shift_factor = Integer(gets.chomp)
-  rescue ArgumentError
-    puts "Invalid input. Enter an integer"
-    retry
-  end
+begin
+  print 'Enter right shift factor: '
+  shift_factor = Integer(gets.chomp)
+rescue ArgumentError
+  puts 'Invalid input. Enter an integer'
+  retry
+end
 
-  puts "\nEncrypted message: #{encrypt_message(message_encrypt, shift_factor)}"
+puts "\nEncrypted message: #{encrypt_message(message_encrypt, shift_factor)}"
